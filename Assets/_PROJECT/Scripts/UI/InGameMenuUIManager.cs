d3335cad67f96d788f8cba9033918faef518634d
Con Momberg <con@fuzzylogicstudio.io>
2017-01-21 19:35:06 +0200

    [C] Further UI work.

---
+++ Assets/_PROJECT/Scripts/UI/InGameMenuUIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace GrimWaves.UI
+{
+	public class InGameMenuUIManager : MonoBehaviour 
+	{
+		#region UNITY EVENTS
+		void Update()
+		{
+			if (Input.GetKeyUp(KeyCode.Escape))
+			{
+				ResumeGame();
+			}
+		}
+		#endregion
+
+
+		#region PUBLIC API
+		public void ResumeGame()
+		{
+			GameManager.instance.ResumeGame();
+		}
+
+		public void QuitLevel()
+		{
+			GameManager.instance.QuitLevel();
+		}
+		#endregion
+	}
+}
