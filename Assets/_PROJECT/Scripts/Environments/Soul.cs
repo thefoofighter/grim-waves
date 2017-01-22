618729be1e7e494dd29a8ca0ec5b27dc6dd04e9e
Con Momberg <con@fuzzylogicstudio.io>
2017-01-22 11:24:40 +0200

    [A] Finished object spawner and added the ability for the ferry to pick up souls.

---
+++ Assets/_PROJECT/Scripts/Environments/Soul.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using GrimWaves.Player;
+
+namespace GrimWaves.Environments
+{
+	public class Soul : MonoBehaviour
+	{
+		#region UNITY EVENTS
+		void OnTriggerEnter(Collider col)
+		{
+			if (col.gameObject.layer == Layers.FERRY)
+			{
+				Ferry.instance.PickUpSoul();
+				Destroy(gameObject);
+			}
+		}
+		#endregion
+	}
+}
